d42d6b57e57dbf1f307ad4944e77a2df8e02c2b1
Dhananjay <dhananjaybhardwaj1416@gmail.com>
2026-09-01 17:22:56 +0000

    Add VPC routing and NAT gateway

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -49,3 +49,79 @@ resource "aws_internet_gateway" "this" {
     Name = "production-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private_app" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "private-app-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_app" {
+  count = length(var.private_app_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private_app[count.index].id
+  route_table_id = aws_route_table.private_app.id
+}
+
+resource "aws_route_table" "private_db" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "private-db-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_db" {
+  count = length(var.private_db_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private_db[count.index].id
+  route_table_id = aws_route_table.private_db.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "production-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "production-nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route" "private_app_nat" {
+  route_table_id         = aws_route_table.private_app.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
